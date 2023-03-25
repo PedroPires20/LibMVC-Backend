5b02243bce6b2976eabe9e8185fec912f329f3f5
Pedro <pedrodias504@gmail.com>
2023-03-25 00:26:39 -0300

    Implementing a basic database driver

---
+++ src/database/database_driver.ts
@@ -0,0 +1,166 @@
+import { MongoClient, Collection } from "mongodb";
+import { DatabaseConnectionError, DriverNotConnected, NoActiveCollection, DatabaseError } from "./driver_exceptions";
+
+export default class DatabaseDriver {
+    constructor(serverAddress: string, serverPort: number, databaseName: string) {
+        this._serverAddress = serverAddress;
+        this._serverPort = serverPort;
+        this._databaseName = databaseName;
+        this._connectionString = `mongodb://${this._serverAddress}:${this._serverPort}/${this._databaseName}`;
+        this._mongoClient = new MongoClient(this._connectionString);
+        this._isConnected = false;
+        this._activeCollectionName = "";
+    }
+
+    public async connect() {
+        try {
+            this._mongoClient.connect();
+            this._isConnected = true;
+        }catch(exception: any) {
+            throw new DatabaseConnectionError(this._serverAddress, this._serverPort, exception.message);
+        }
+    }
+
+    public async disconnect(forceDisconnect = false) {
+        try {
+            await this._mongoClient.close(forceDisconnect);
+        }catch(exception: any) {
+            throw new DatabaseError(this._connectionString, "Failed to close connection with the database!\nThe following error was encountered: " + exception.message);
+        }
+    }
+
+    public listCollections() {
+        if(!this._isConnected) {
+            throw new DriverNotConnected(this._connectionString);
+        }
+        let collectionNames: string[] = [];
+        this._mongoClient.db().listCollections({}, { nameOnly: true }).forEach((collectionInfo) => {
+            collectionNames.push(collectionInfo.name)
+        });
+        return collectionNames;
+    }
+
+    public async createCollection(newCollectionName: string) {
+        if(!this._isConnected) {
+            throw new DriverNotConnected(this._connectionString);
+        }
+        await this._mongoClient.db().createCollection(newCollectionName);
+    }
+
+    public async dropCollection(collectionName: string) {
+        if(!this._isConnected) {
+            throw new DriverNotConnected(this._connectionString);
+        }
+        await this._mongoClient.db().dropCollection(collectionName);
+    }
+    
+    public get activeCollection() {
+        return this._activeCollectionName;
+    }
+
+    public set activeCollection(collectionName: string) {
+        this._activeCollectionName = collectionName;
+        this._activeCollection = this._mongoClient.db().collection(this._activeCollectionName);
+    }
+
+    public get find() {
+        if(!this._isConnected) {
+            throw new DriverNotConnected(this._connectionString);
+        }
+        if(!this._activeCollection) {
+            throw new NoActiveCollection(this._connectionString);
+        }
+        return this._activeCollection.find.bind(this._activeCollection);
+    }
+
+    public get findOne() {
+        if(!this._isConnected) {
+            throw new DriverNotConnected(this._connectionString);
+        }
+        if(!this._activeCollection) {
+            throw new NoActiveCollection(this._connectionString);
+        }
+        return this._activeCollection.findOne.bind(this._activeCollection);
+    }
+
+    public get insertOne() {
+        if(!this._isConnected) {
+            throw new DriverNotConnected(this._connectionString);
+        }
+        if(!this._activeCollection) {
+            throw new NoActiveCollection(this._connectionString);
+        }
+        return this._activeCollection.insertOne.bind(this._activeCollection);
+    }
+
+    public get insertMany() {
+        if(!this._isConnected) {
+            throw new DriverNotConnected(this._connectionString);
+        }
+        if(!this._activeCollection) {
+            throw new NoActiveCollection(this._connectionString);
+        }
+        return this._activeCollection.insertMany.bind(this._activeCollection);
+    }
+
+    public get updateOne() {
+        if(!this._isConnected) {
+            throw new DriverNotConnected(this._connectionString);
+        }
+        if(!this._activeCollection) {
+            throw new NoActiveCollection(this._connectionString);
+        }
+        return this._activeCollection.updateOne.bind(this._activeCollection);
+    }
+
+    public get updateMany() {
+        if(!this._isConnected) {
+            throw new DriverNotConnected(this._connectionString);
+        }
+        if(!this._activeCollection) {
+            throw new NoActiveCollection(this._connectionString);
+        }
+        return this._activeCollection.updateMany.bind(this._activeCollection);
+    }
+
+    public get deleteOne() {
+        if(!this._isConnected) {
+            throw new DriverNotConnected(this._connectionString);
+        }
+        if(!this._activeCollection) {
+            throw new NoActiveCollection(this._connectionString);
+        }
+        return this._activeCollection.deleteOne.bind(this._activeCollection);
+    }
+
+    public get deleteMany() {
+        if(!this._isConnected) {
+            throw new DriverNotConnected(this._connectionString);
+        }
+        if(!this._activeCollection) {
+            throw new NoActiveCollection(this._connectionString);
+        }
+        return this._activeCollection.deleteMany.bind(this._activeCollection);
+    }
+    
+    public get connectionString() {
+        return this._connectionString;
+    }
+
+    public get databaseName() {
+        return this._databaseName;
+    }
+
+    public get isConnected() {
+        return this._isConnected;
+    }
+
+    private _serverAddress: string;
+    private _serverPort: number;
+    private _databaseName: string;
+    private _connectionString: string;
+    private _mongoClient: MongoClient;
+    private _isConnected: boolean;
+    private _activeCollectionName: string;
+    private _activeCollection?: Collection;
+}
---
+++ src/database/driver_exceptions.ts
@@ -0,0 +1,25 @@
+import { ApiError } from "../helpers/error_handlers";
+
+export class DatabaseConnectionError extends ApiError {
+    constructor(serverAddress: string, serverPort: number, errorMessage: string) {
+        super(`Failed to connect to the MongoDb server at URL "${serverAddress}", port ${serverPort}!\nThe following error was encountered:\n${errorMessage}`, 500, "DatabaseDriver");
+    }
+}
+
+export class DatabaseError extends ApiError {
+    constructor(connectionString: string, errorMessage: string) {
+        super(`The MongoDB server at "${connectionString}" encountered the following error:\n${errorMessage}`, 500, "DatabaseDriver");
+    }
+}
+
+export class DriverNotConnected extends ApiError {
+    constructor(connectionString: string) {
+        super(`The database driver didn't initialize a connection to the MongoDB server or the connection was lost/closed\nConnection string: ${connectionString}`, 500, "DatabaseDriver");
+    }
+}
+
+export class NoActiveCollection extends ApiError {
+    constructor(connectionString: string) {
+        super(`The current database driver has no active collection! Please ensure that a collection was set before making queries...\nConnection string: ${connectionString}`, 500, "DatabaseDriver");
+    }
+}
