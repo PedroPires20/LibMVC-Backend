273e7d7c6114739ab81d9783a88e773b51ef2305
Pedro <pedrodias504@gmail.com>
2023-04-02 01:07:22 -0300

    Encapsulating the API setup/serve in an App class

---
+++ src/app.ts
@@ -0,0 +1,68 @@
+import express, { Express } from "express";
+import * as dotenv from "dotenv";
+import { exceptionHandler, notFoundHandler } from "./helpers/error_handlers";
+import { Controller, DatabaseSettings } from "./controllers/controller";
+import BookController from "./controllers/book_controller";
+
+const DEFAULT_PORT_NUMBER = 3000;
+
+export default class App {
+    constructor() {
+        this._port = 0;
+        this._expressApp = express();
+        this._databaseSettings = { serverUrl: "", serverPort: 0, databaseName: "" };
+        this._routeControllers = new Map();
+    }
+
+    public initialize() {
+        this._readEnvVariables();
+        this._initializeControllers();
+        this._addPreHandlerMiddlewares();
+        this._bindRoutes();
+        this._addPostHandlerMiddlewares();
+    }
+
+    public listen() {
+        this._expressApp.listen(this._port, () => {
+            console.log(`App listening on port ${this._port}...`);
+        });
+    }
+
+    private _readEnvVariables() {
+        dotenv.config();
+        this._port = parseInt(process.env.PORT || "");
+        if(isNaN(this._port)) {
+            this._port = DEFAULT_PORT_NUMBER;
+        }
+        this._databaseSettings.serverUrl = process.env.MONGO_URL || "";
+        this._databaseSettings.serverPort = parseInt(process.env.MONGO_PORT || "");
+        this._databaseSettings.databaseName = process.env.MONGO_DATABASE || "";
+    }
+
+    private _initializeControllers() {
+        this._routeControllers.set("/books", new BookController(this._databaseSettings));
+        for(let controller of this._routeControllers.values()) {
+            controller.initializeController();
+        }
+    }
+
+    private _addPreHandlerMiddlewares() {
+        this._expressApp.use(express.json());
+    }
+
+    private _bindRoutes() {
+        for(const [route, controller] of this._routeControllers) {
+            controller.bindRoute(this._expressApp, route);
+        }
+    }
+
+    private _addPostHandlerMiddlewares() {
+        this._expressApp.use(notFoundHandler);
+        this._expressApp.use(exceptionHandler);
+    }
+
+    private _port: number;
+    private _expressApp: Express;
+    private _databaseSettings: DatabaseSettings;
+    private _routeControllers: Map<string, Controller>;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
-import express, { Request, Response, NextFunction } from "express";
-import { exceptionHandler, notFoundHandler } from "./helpers/error_handlers";
+import App from "./app";
 
-// Initializing application components
-const apiPort = process.env.PORT || 3000;
-const app = express();
+const app = new App();
 
-// Adding middlewares
-app.use(express.json());
-app.use(exceptionHandler);
+app.initialize();
 
-// Defining the routes
-app.get("/", (req, res) => { res.status(200).json({ message: `Hello, ${req.body.name}!` }); console.log("Received a request!"); });
-
-// Adding the handler for missing routes (404)
-app.use(notFoundHandler);
-
-// Starting the app and listening for requests
-app.listen(apiPort, () => {
-    console.log(`App listening on port ${apiPort}...`);
-});
+app.listen();
