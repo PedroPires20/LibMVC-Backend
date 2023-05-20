f9c39db83a0ff1e220e3543dcaaea945eb36a215
Pedro <pedrodias504@gmail.com>
2023-05-20 14:52:14 -0300

    Fixing a CORS error

--- src/helpers/enable_cors.ts
+++ src/helpers/enable_cors.ts
@@ -7,7 +7,7 @@ export function enableCors(
     next: NextFunction
 ) {
     response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
     response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 }
